4584b15b5b6c691166c1f1dc83b21f2c90f43924
Adarsh-Kumar1108 <adarsh8709392288@gmail.com>
2026-03-10 22:38:16 +0530

    Add files via upload

---
+++ Adarsh/script.js
@@ -0,0 +1,36 @@
+function scrollToSection(id){
+
+document.getElementById(id).scrollIntoView({
+behavior:"smooth"
+})
+
+}
+
+
+const sections = document.querySelectorAll(".section")
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(section=>{
+
+const top = section.getBoundingClientRect().top
+
+if(top < window.innerHeight - 120){
+
+section.style.opacity="1"
+section.style.transform="translateY(0)"
+
+}
+
+})
+
+})
+
+
+sections.forEach(section=>{
+
+section.style.opacity="0"
+section.style.transform="translateY(50px)"
+section.style.transition="all 0.8s ease"
+
+})
